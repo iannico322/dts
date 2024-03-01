2672e8f4dd9093dc382eab270620a809a3236409
iannico322 <iannicocaulin@gmail.com>
2024-03-01 16:06:09 +0900

    fix : navbar

--- src/components/nav/dropdown/Dropdown.tsx
+++ src/components/nav/dropdown/Dropdown.tsx
@@ -24,7 +24,7 @@ const Dropdown = ({head,lists,headLogo,direct}:any) => {
              }
              
             }
-           className="hover:bg-[#bbbbbb4f] text-[#1B4B91]  hover:font-normal transition-colors  w-full flex cursor-pointer  py-2 rounded-sm justify-between px-2 " 
+           className={!expand?"hover:bg-[#bbbbbb4f] text-[#1B4B91]  hover:font-normal transition-colors  w-full flex cursor-pointer  py-2 rounded-sm justify-between px-2": "hover:bg-[#bbbbbb11] bg-[#bbbbbb4f] text-[#1B4B91]  hover:font-normal transition-colors  w-full flex cursor-pointer  py-2 rounded-sm justify-between px-2"}
          >
            <div className=" font-light flex items-center justify-between w-full ">
                <div className=" flex gap-3">
@@ -77,7 +77,7 @@ const Dropdown = ({head,lists,headLogo,direct}:any) => {
              }
              
             }
-           className="hover:bg-[#bbbbbb4f] text-[#1B4B91]  hover:font-normal transition-colors  w-full flex cursor-pointer  py-2 rounded-sm justify-between px-2 " 
+           className={!expand?"hover:bg-[#bbbbbb4f] text-[#1B4B91]  hover:font-normal transition-colors  w-full flex cursor-pointer  py-2 rounded-sm justify-between px-2": "hover:bg-[#bbbbbb11] bg-[#bbbbbb4f] text-[#1B4B91]  hover:font-normal transition-colors  w-full flex cursor-pointer  py-2 rounded-sm justify-between px-2"}
          >
            <div className=" font-light flex items-center justify-between w-full ">
                <div className=" flex gap-3">
@@ -86,35 +86,10 @@ const Dropdown = ({head,lists,headLogo,direct}:any) => {
                <p className={!expand?" font-normal md:hidden": "font-semibold md:hidden"}>{head}</p>
                </div>
                
-               { !(lists.length === 0)?  !expand? <ChevronLeft className=" h-4 w-4 text-[#1B4B91]"/> : < ChevronDown className="text-[#1B4B91] h-4 w-4" /> :""}
            </div>
            
          </div>
-         {
-           !(lists.length === 0)?
-           <div className={    expand? " text-[#1B4B91] flex flex-col  relative  translate-x-[0%]  pl-6 space-y-2 opacity-100 transition-all duration-500 ease-in-out gap-4 ":" flex flex-col text-[#1B4B91]  translate-x-[-10%] translate-y-[-0%] opacity-0 pointer-events-none gap-4  absolute " }>
-         {lists.map((e:any,key:any)=>(
-               <div
-               onClick={
-                 ()=>{
-                   
-                   navigate(e.link)
-                 }
-               }
-               key={key}
-               className=" m-0 flex w-full px-5   transition-colors rounded-sm cursor-pointer hover:bg-opacity-90 hover:font-semibold  items-center gap-2"
-             >
-               <img src={e.icon} className=" m-0 h-5 object-contain" alt={e.li} />
-               <h1 className=" m-0 md:hidden">{e.li}</h1> 
-             </div>
-   
-           ))}
-          
-         </div>
-           
-           
-           :""
-         }
+        
          
        </Link>
     }
