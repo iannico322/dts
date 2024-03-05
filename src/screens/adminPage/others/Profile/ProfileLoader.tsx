1ba0ab599bca7e1201f2af7ed11cd8de35ac3d4f
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-03-05 10:36:09 +0800

    improve skeleton loader for admin profile

--- src/screens/adminPage/others/Profile/ProfileLoader.tsx
+++ src/screens/adminPage/others/Profile/ProfileLoader.tsx
@@ -1,4 +1,5 @@
 
+import profile from '../../../../assets/images/profileSkeleton.png'
 const ProfileLoader = () => {
 
 
@@ -8,31 +9,37 @@ const ProfileLoader = () => {
         {/* left side */}
         <div className=" col-span-4 w-full h-full pr-32 ">
 
+            <div className=" h-[40px] w-[250px] bg-slate-400 rounded-md animate-pulse mb-5"></div>
 
             {/* left top */}
-            <div className=" flex flex-row h-[200px] w-full justify-between items-center pl-10">
+            <div className=" flex flex-row h-[200px] w-full justify-between items-center pl-4">
 
                 {/* admin name, email, profile pic */}
-                <div className=" flex flex-row items-center w-[440px] h-full ">
+                <div className=" flex flex-row items-center w-[440px] h-full">
 
+                    <img className=' animate-pulse ' src={profile} alt='profileSkeleton'/>
 
-                    <div className="flex flex-col w-[400px] h-[90px] lg:h-[80px] bg-slate-400 rounded-xl p-4 pl-10 translate-x-[-30px] animate-pulse">
-
+                    {/* <div className=" w-[140px] h-[140px]">
+                        <div className=" w-full h-full rounded-full bg-slate-400 "></div>
                     </div>
+                    <div className="flex flex-col w-[320px] h-[90px] lg:h-[80px] bg-slate-400 rounded-xl p-4 pl-10 translate-x-[-30px] ">
+
+                    </div> */}
                 </div>
 
                 {/* admin password */}
-                <div className=" flex flex-col w-[440px] h-[90px] lg:h-[80px] bg-slate-400 rounded-xl p-4 pl-8 animate-pulse">
+                <div className=" flex flex-col w-[320px] h-[90px] lg:h-[80px] bg-slate-400 rounded-xl p-4 pl-8 mt-2 animate-pulse">
 
                 </div>
             </div>
 
 
             {/* General Information */}
-            <div className=" flex flex-col w-full min-h-[500px] mt-10 pl-10 bg-slate-400 rounded-md animate-pulse">
+            <div className=" flex flex-col w-full h-[500px] mt-10 pl-10 ">
 
+                <div className=" h-[45px] w-[250px] bg-slate-400 rounded-md animate-pulse mb-5"></div>
                 {/* container */}
-                <div className=" flex flex-wrap justify-between w-full h-full  ">
+                <div className=" flex w-full h-full bg-slate-400 rounded-md animate-pulse  ">
 
 
                 </div>
