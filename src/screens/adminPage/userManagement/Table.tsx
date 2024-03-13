df8142a72f987d967e2e318a2b4003ab81f76deb
iannico322 <iannicocaulin@gmail.com>
2024-03-13 14:18:21 +0800

    done userlist

--- src/screens/adminPage/userManagement/Table.tsx
+++ src/screens/adminPage/userManagement/Table.tsx
@@ -3,6 +3,7 @@
 import { useTable, useSortBy, usePagination, useGlobalFilter } from 'react-table';
 import data from './userData.json'
 import { useEffect } from 'react';
+import { PencilIcon, TrashIcon } from 'lucide-react';
 
 
 
@@ -66,7 +67,7 @@ function MyTable({search}:any) {
   const { pageIndex}:any = state;
 
   useEffect(()=>{
-    setPageSize(5)
+    setPageSize(10)
   },[])
 
   useEffect(()=>{
@@ -74,45 +75,100 @@ function MyTable({search}:any) {
   },[search])
 
   return (
-    <div className=' flex items-center flex-col'>
-      <table {...getTableProps()} className='  w-full text-left text-xs font-light'>
-        <thead className=' text-violet1'>
-          {headerGroups.map((headerGroup:any) => (
-            <tr {...headerGroup.getHeaderGroupProps()} className='  border-b  border-[#00000013] text-sm'>
-              {headerGroup.headers.map((column:any) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())} className=' pl-4 py-2'>
-                  {column.render('Header')}
-                  <span>
-                    {column.isSorted ? (
-                      column.isSortedDesc ? (
-                        <span>▼</span>
-                      ) : (
-                        <span>▲</span>
-                      )
-                    ) : (
-                      ''
-                    )}
-                  </span>
-                </th>
-              ))}
-            </tr>
+    <div className=' flex flex-col h-full w-full justify-between'>
+
+      <div className='overflow-hidden'>
+  <table {...getTableProps()} className='w-full text-xs font-light table-fixed'>
+    <thead className='text-violet1 h-[20px]'>
+      {headerGroups.map((headerGroup:any) => (
+        <tr {...headerGroup.getHeaderGroupProps()} className='border-b border-[#00000013] text-sm'>
+          {headerGroup.headers.map((column:any) => (
+            <th
+              {...column.getHeaderProps(column.getSortByToggleProps())}
+              className='pl-4 py-2 text-left truncate' // Add truncate class
+            >
+              {column.render('Header')}
+              <span>
+                {column.isSorted ? (
+                  column.isSortedDesc ? (
+                    <span>▼</span>
+                  ) : (
+                    <span>▲</span>
+                  )
+                ) : (
+                  ''
+                )}
+              </span>
+            </th>
           ))}
-        </thead>
-        <tbody {...getTableBodyProps()} className='  '>
-          {page.map((row:any) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()} className=' hover:bg-blue3/10  h-10   border-b  border-[#00000013] text-sm font-medium'>
-                {row.cells.map((cell:any) => (
-                  <td {...cell.getCellProps()} className=' h-10   pl-4 py-4'>{cell.render('Cell')}</td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <div className=' absolute bottom-0 self-center mb-5 flex justify-center gap-4 mt-10 bg-blueTable-100 text-center'>
-      <button  onClick={() => previousPage()} disabled={!canPreviousPage} className='className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
+        </tr>
+      ))}
+    </thead>
+  </table>
+  <div className='overflow-y-auto overflow-x-hidden max-h-[300px]'>
+    <table className='w-full text-xs font-light table-fixed'>
+      <tbody {...getTableBodyProps()} className='overflow-hidden w-full'>
+      {page.map((row:any) => {
+        prepareRow(row);
+        return (
+          <tr
+            {...row.getRowProps()}
+            className='hover:bg-blue3/10 h-10 border-b border-[#00000013] text-sm font-medium'
+          >
+            {row.cells.map((cell:any) => 
+
+              (
+                !(cell.column.id == "status")?<td key={cell.column.id} className='h-10 pl-4 py-4 text-left truncate'> {/* Add truncate class */}
+                {cell.render('Cell')}
+              </td>:
+               <td  key={cell.column.id} className=" h-full w-full  items-center   py-4 sm:py-2 text-center flex gap-3 sm:gap-1 justify-center sm:flex-col">
+               <button className='rounded-[4px]  border-[1px] text-xs border-textY bg-bgY text-textY font-md px-2 py-1 sm:py-1 sm:px-1 hover:bg-opacity-50 sm:items-center sm:justify-center'> 
+                  <PencilIcon className=' h-2 w-2 hidden sm:flex'/>
+                  <p className=' sm:hidden '>UPDATE</p>  
+               </button>
+               <button className=' rounded-[4px] border-[1px] hover:bg-opacity-50 text-xs border-textR bg-bgR text-textR font-md px-2 py-1 sm:px-1 sm:py-1'> 
+               <TrashIcon className=' h-2 w-2 hidden sm:flex'/>
+                  <p className=' sm:hidden truncate'>DEACTIVATE</p>
+                  
+               </button>
+           </td>
+              )
+              
+              
+            )}
+          </tr>
+        );
+      })}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<div className=' hidden sm:flex  h-[10%] k w-full items-center bottom-0 self-center mb-2  justify-center gap-4 sm:gap-2 mt-2 bg-blueTable-100 text-center'>
+      <button  onClick={() => previousPage()} disabled={!canPreviousPage} className='className="flex items-center gap-2 px-6 sm:px-3 sm:p-3 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
+                                    disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            aria-hidden="true" className="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
+                            </svg>
+  </button>
+ 
+ <p className=' text-xs'> <b>{pageIndex +1} </b> / {pageCount }</p>
+  
+
+  <button onClick={() => nextPage()} disabled={!canNextPage}
+                                    className="flex items-center gap-2 px-6 sm:px-3 sm:p-3 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
+                                    disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                    type="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                    aria-hidden="true" className="w-4 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+                                    </svg>
+                                </button>
+      </div>  
+
+    <div className=' sm:hidden  h-[10%] k w-full items-center bottom-0 self-center mb-2 flex justify-center gap-4 sm:gap-2 mt-2 bg-blueTable-100 text-center'>
+      <button  onClick={() => previousPage()} disabled={!canPreviousPage} className='className="flex items-center gap-2 px-6 sm:px-3 sm:p-3 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
                                     disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"'>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
                             aria-hidden="true" className="w-4 h-4">
@@ -127,7 +183,7 @@ function MyTable({search}:any) {
       <span className='px-3 py-1'>...</span>
     </>
   )}
-  {Array.from({ length: Math.min(5, pageCount) }, (_, index) => {
+  {Array.from({ length: Math.min(4, pageCount) }, (_, index) => {
     const pageNumber = pageIndex - 2 + index;
     if (pageNumber < 0 || pageNumber >= pageCount) return null;
     return (
@@ -151,7 +207,7 @@ function MyTable({search}:any) {
   
 
   <button onClick={() => nextPage()} disabled={!canNextPage}
-                                    className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
+                                    className="flex items-center gap-2 px-6 sm:px-3 sm:p-3 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
                                     disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                                     type="button">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
--- src/screens/adminPage/userManagement/Users.tsx
+++ src/screens/adminPage/userManagement/Users.tsx
@@ -37,25 +37,25 @@ const Users = () => {
     
   return (
     <div className='  w-full h-full bg-blue3 flex items-center justify-center'>
-        <div className=" w-[90%] h-[80%] bg-white rounded-md drop-shadow-glow flex flex-col p-10 ">
-            <div className='flex flex-row items-start justify-between'>
+        <div className=" w-[90%] h-[80%] sm:h-[95%] sm:w-[90%]  overflow-hidden bg-white rounded-md drop-shadow-glow flex flex-col p-10 sm:p-5 gap-10 sm:gap-5 ">
+            <div className=' sm:gap-5 flex flex-row sm:flex-col items-center w-full sm:items-start justify-between'>
                 <div className='div1'>
-                    <h1 className="text-black font-poppins text-l font-semibold leading-normal tracking-tight"> User Management </h1>
-                    <p className="text-teal-500 font-poppins text-m text-base font-normal leading-normal tracking-tight mb-10"> Active Members</p>
+                    <h1 className="text-black font-poppins font-bold  w-[200px] text-lg eading-normal tracking-tight"> User Management </h1>
+                    <p className="text-teal-500 font-poppins text-m text-base font-normal leading-normal tracking-tight "> Active Members</p>
                 </div>
 
-                <div className='div2 space-x-7 flex items-center'>
+                <div className='  flex sm:flex-col items-center gap-5 sm:justify-between w-full justify-end  relative sm:gap-5 sm:items-start  '>
                     <button className='
-                        rounded-[5px] border-2 border-textG bg-bgG  text-textG px-5 py-1 font-medium text-xs
+                        rounded-[5px] truncate border-2 border-textG bg-bgG  text-textG px-5 py-1 font-medium text-xs
                     '> + Add User
                     </button> 
 
-                    <div className="relative px-3 py-1 border-border border  h-[18] flex items-center w-[505] bg-bgW rounded-md ">
+                    <div className="relative px-3 py-1 border-border border  flex items-center w-[80%] sm:w-full bg-bgW rounded-md m-0  max-w-[200px] sm:max-w-full">
                       <input
                         value={search}
                         onChange={handleSearch}
                         placeholder="Search..."
-                        className="outline-none bg-bgW text-xs pl-7 text-gray-900 py-1 "
+                        className="outline-none w-full bg-bgW text-xs pl-7 text-gray-900 py-1 "
                         type="search"
                       />
                       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none mr-7px-4">
