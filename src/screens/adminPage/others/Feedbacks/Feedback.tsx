4b9ae0684d64e773d76893459cafe9fa85d11535
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-03-15 13:20:43 +0800

    Pagination and  sorting using react table

--- src/screens/adminPage/others/Feedbacks/Feedback.tsx
+++ src/screens/adminPage/others/Feedbacks/Feedback.tsx
@@ -1,68 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { CheckCircle, ChevronDown, FileWarning, TrashIcon } from "lucide-react";
-import { useState } from "react";
+import { CheckCircle, TrashIcon } from "lucide-react";
+import FeedbacksTable from "./FeedbacksTable/Table";
 
 
 function Feedback() {
-
-    const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
-
-    const handleCheckboxChange = (index: number) => {
-        // Create a copy of the checkedItems array
-        const updatedCheckedItems = [...checkedItems];
-        updatedCheckedItems[index] = !updatedCheckedItems[index]; // Toggle the checked status
-
-        setCheckedItems(updatedCheckedItems);
-    };
-
-    const tickets = [
-        {"Ticket_Title" : "Hello", "Category": "Concern", "Email": "user1@gmail.com", "Date": "January 23, 2021", "Status": "Done"},
-        {"Ticket_Title" : "Marketplace", "Category": "Concern", "Email": "user2@gmail.com", "Date": "February 24, 2021", "Status": "Pending"},
-        {"Ticket_Title" : "Venus PRO", "Category": "Positive", "Email": "user3@gmail.com", "Date": "January 12, 1999", "Status": "Pending"},
-        {"Ticket_Title" : "Uranus Kit", "Category": "Okay", "Email": "user4@gmail.com", "Date": "May 29, 2019", "Status": "Pending"},
-        {"Ticket_Title" : "Mars Rover", "Category": "Urgent", "Email": "user5@gmail.com", "Date": "March 14, 2022", "Status": "Done"},
-        {"Ticket_Title" : "Jupiter Flyby", "Category": "Inquiry", "Email": "user6@gmail.com", "Date": "April 18, 2023", "Status": "Pending"},
-        {"Ticket_Title" : "Saturn Rings", "Category": "Positive", "Email": "user7@gmail.com", "Date": "June 21, 2021", "Status": "Done"},
-        {"Ticket_Title" : "Neptune Deep Dive", "Category": "Okay", "Email": "user8@gmail.com", "Date": "July 30, 2020", "Status": "Pending"},
-        {"Ticket_Title" : "Pluto Discovery", "Category": "Inquiry", "Email": "user9@gmail.com", "Date": "August 24, 2024", "Status": "Done"},
-        {"Ticket_Title" : "Mercury Mission", "Category": "Urgent", "Email": "user10@gmail.com", "Date": "September 30, 2023", "Status": "Pending"},
-        {"Ticket_Title" : "Earth Day", "Category": "Positive", "Email": "user11@gmail.com", "Date": "April 22, 2024", "Status": "Done"},
-        {"Ticket_Title" : "Galaxy Exploration", "Category": "Inquiry", "Email": "user12@gmail.com", "Date": "October 1, 2024", "Status": "Pending"},
-        {"Ticket_Title" : "Black Hole Study", "Category": "Urgent", "Email": "user13@gmail.com", "Date": "November 2, 2024", "Status": "Done"},
-        {"Ticket_Title" : "Comet Chase", "Category": "Positive", "Email": "user14@gmail.com", "Date": "December 3, 2024", "Status": "Pending"},
-        {"Ticket_Title" : "Asteroid Mining", "Category": "Inquiry", "Email": "user15@gmail.com", "Date": "January 4, 2025", "Status": "Done"},
-        {"Ticket_Title" : "Solar Flare Forecast", "Category": "Urgent", "Email": "user16@gmail.com", "Date": "February 5, 2025", "Status": "Pending"},
-        {"Ticket_Title" : "Moon Landing Anniversary", "Category": "Positive", "Email": "user17@gmail.com", "Date": "July 20, 2025", "Status": "Done"},
-        {"Ticket_Title" : "Space Debris Tracking", "Category": "Inquiry", "Email": "user18@gmail.com", "Date": "March 6, 2025", "Status": "Pending"},
-        {"Ticket_Title" : "Alien Life Search", "Category": "Urgent", "Email": "user19@gmail.com", "Date": "April 7, 2025", "Status": "Done"},
-        {"Ticket_Title" : "Interstellar Travel", "Category": "Positive", "Email": "user20@gmail.com", "Date": "May 8, 2025", "Status": "Pending"},
-    ]
-
-    // pagination
-
-    // creating a new array with values based on the length, index value plus 1
-    // e.g (length is 5 so newArray = [1, 2, 3 , 4, 5]) 
-    const data = Array.from({ length: tickets.length }, ( k:any) => k+1);
-
-    // Change page
-    const [currentPage, setCurrentPage] = useState(0);
-    const handlePageClick = (_event:any, value:any) => {
-
-      setCurrentPage(value);
-    };
-  
-    const nextPage = () => {
-      if (currentPage < Math.ceil(data.length / 8) - 1) {
-        setCurrentPage(currentPage + 1);
-      }
-    };
-  
-    const prevPage = () => {
-      if (currentPage > 0) {
-        setCurrentPage(currentPage - 1);
-      }
-    };
-    
 
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
@@ -96,7 +37,9 @@ function Feedback() {
                     {/* table */}
                     <div className=" flex w-full px-8 ">
 
-                        <table className="table-auto w-full ">
+                        <FeedbacksTable />
+
+                        {/* <table className="table-auto w-full ">
 
                             <thead className=" border-b-2 ">
                                 <tr>
@@ -171,58 +114,14 @@ function Feedback() {
 
                             </tbody>
 
-                        </table>
+                        </table> */}
 
 
                     </div>
 
 
-                {/* PAGINATION */}
-                    <div className="  flex justify-center gap-4 mt-5 bg-blueTable-100 text-center ">
-
-                        {/* prev button */}
-                        <button
-                            onClick={prevPage}
-                            disabled = {currentPage === 0}
-                            className= "flex items-center gap-2 px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-orange active:bg-orange disabled:bg-transparent disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none"
-                            type="button">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                            aria-hidden="true" className="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
-                            </svg>
-                        </button>
-
-                        {/* mapping the page numbers */}
-                        <div className="flex items-center gap-2">
-                        {Array.from({ length: Math.ceil(data.length / 8) }, (_v, k) => k).map((page) => (
-                            <button
-                                key={page}
-                                onClick={(event) => handlePageClick(event, page)}
-                                className={`h-9 max-h-[40px] w-9 max-w-[40px] select-none rounded-lg  text-center align-middle font-sans text-xs font-medium uppercase text-black shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-orange focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ${currentPage === page ? 'active-page-class bg-orange text-white' : ''}`}
-                                type="button">
-                                <span>{page + 1}</span>
-                            </button>
-                        ))}
-                        
-                        </div>
-                                
-                        {/* next button */}
-                        <button
-                            onClick={nextPage}
-                            disabled={currentPage === Math.ceil(data.length / 8) - 1}
-                            className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
-                            disabled:bg-transparent disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none"
-                            type="button">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                            aria-hidden="true" className="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
-                            </svg>
-                        </button>
-
-                    </div>
                 </div>
             </div>
-
         </div>
     </ThemeProvider>
   )
---
+++ src/screens/adminPage/others/Feedbacks/FeedbacksTable/Table.tsx
@@ -0,0 +1,294 @@
+import DATA from './data.json'
+import { useEffect, useState } from 'react'
+import { useTable, usePagination, useSortBy } from 'react-table'
+import { CaretDownIcon, CaretUpIcon } from '@radix-ui/react-icons'
+
+
+const columns:any = [
+    {
+      Header: 'Ticket Title',
+      accessor: 'ticket_title'
+    },
+    {
+      Header: 'Category',
+      accessor: 'category'
+    },
+    {
+      Header: 'Email',
+      accessor: 'email'
+    },
+    {
+      Header: 'Date',
+      accessor: 'date'
+    },
+    {
+      Header: 'Status',
+      accessor: 'status'
+    },
+]
+
+const data = DATA
+
+export default function FeedbacksTable (){
+
+    const [checkedState, setCheckedState] = useState<boolean[]>([])
+
+    useEffect(()=>{
+        console.log(data)
+    },[])
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        previousPage,
+        nextPage,
+        canPreviousPage,
+        canNextPage,
+        pageCount,
+        gotoPage,
+        setPageSize,
+        state,
+      }:any = useTable(
+        {
+          columns,
+          data,
+        },
+        useSortBy,
+        usePagination
+      );
+
+      const { pageIndex}:any = state;
+
+      useEffect(()=>{
+        setPageSize(7)
+      },[])
+
+    return(
+        <div className=' flex flex-col w-full h-full justify-between'>
+
+            <div className='w-full h-[30px] border-b-[2px]'>
+                <table {...getTableProps()} className=' w-full '>
+
+                    <thead>
+
+                        {
+                            headerGroups.map((headerGroup:any)=> (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                {
+                                    headerGroup.headers.map((column:any, i:any) => {
+                                            
+                                    const {
+                                        getSortByToggleProps,
+                                        // isSorted,
+                                        // isSortedDesc,
+                                    } = column;
+                            
+                                    // Use the sorting state to determine the sort direction icon
+                                    //let icon = isSorted ? (isSortedDesc ? ' ' : ' ') : '';
+                                    let widthClass = '';
+                                    //const [isToggled, setIsToggled] = useState<boolean>(false);
+                                    // const sortFn = () => {
+                                    //     setIsToggled(!isToggled);
+                                    //     handleColumnClick(i); 
+                                    //   };
+                                    // let icon = isToggled ? <CaretUpIcon onClick={sortFn} className='ml-3 w-[25px] h-[25px]' /> : <CaretDownIcon onClick={sortFn} className='ml-3 w-[25px] h-[25px]' />;
+                                    switch(
+                                        i) {
+                                        case 0:
+                                        widthClass = 'w-[30%]';
+                                        break;
+                                        case 1:
+                                        widthClass = 'w-[20%]';
+                                        break;
+                                        case 2:
+                                        widthClass = 'w-[20%]';
+                                        break;
+                                        case 3:
+                                        widthClass = 'w-[20%]';
+                                        break;
+                                        case 4:
+                                        widthClass = 'w-[20%]';
+                                        break;
+                                        default:
+                                        widthClass = 'w-auto';
+                                    }
+                                    return (
+                                        <th {...column.getHeaderProps(getSortByToggleProps())} className={` text-[#436BBE] text-[17px] text-start font-semibold ${widthClass}`}>
+                                            <div className=' flex flex-row '>
+                                                {column.render('Header')}
+                                                <span>
+                                                    {column.isSorted ? (
+                                                    column.isSortedDesc ? (
+                                                        <CaretUpIcon className='ml-1 w-[25px] h-[25px]' />
+                                                    ) : (
+                                                        <CaretDownIcon className='ml-1 w-[25px] h-[25px]' />
+                                                    )
+                                                    ) : (
+                                                    ''
+                                                    )}
+                                                </span>
+                                            </div>
+                                        </th>
+                                    );
+                                    })
+                                }
+                                </tr>
+                            ))
+                        }
+
+                    </thead>
+
+                </table>
+
+            </div>
+
+            <div className='w-full h-[370px] overflow-y-scroll'>
+                <table className=' w-full '>
+                    <tbody {...getTableBodyProps()}>
+
+                        {
+                            page.map((row:any)=>{
+                                prepareRow(row)
+                                return (
+                                    <tr {...row.getRowProps()}>
+
+                                        {row.cells.map((cell:any, i:any)=>{
+
+                                            let widthClass = '';
+                                            switch(i) {
+                                                case 0:
+                                                widthClass = 'w-[30%]';
+                                                break;
+                                                case 1:
+                                                widthClass = 'w-[20%]';
+                                                break;
+                                                case 2:
+                                                widthClass = 'w-[20%]';
+                                                break;
+                                                case 3:
+                                                widthClass = 'w-[20%]';
+                                                break;
+                                                case 4:
+                                                widthClass = 'w-[20%]';
+                                                break;
+                                                default:
+                                                widthClass = 'w-auto';
+                                            }
+
+                                            return <td {...cell.getCellProps()} className= {`text-[#2B3674] font-semibold py-3 ${widthClass}`}> 
+                                                <div className=' w-full flex flex-row items-center'>
+                                                    {i === 0 && 
+                                                        <input 
+                                                        type="checkbox" 
+                                                        className='mr-3 text-white w-5 h-5 cursor-pointer' 
+                                                        checked={!!checkedState[row.id]} 
+                                                        onChange={() => setCheckedState(prev => ({...prev, [row.id]: !prev[row.id]}))}
+                                                        />
+                                                    }                                        
+                                                    {cell.render('Cell')}
+                                                </div>   
+
+                                            </td>
+                                        })}
+
+                                    </tr>
+                                )
+                            })
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div className=' flex justify-center items-center w-full h-[70px] '>
+
+                {/* Previous button */}
+                <button 
+                    onClick={() => previousPage()} 
+                    disabled={!canPreviousPage}
+                    className= "flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:orange active:bg-orange disabled:bg-transparent disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none"
+                    type="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                    aria-hidden="true" className="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
+                    </svg>
+                </button>{' '}
+
+                {/* Page number buttons */}
+
+                {pageIndex > 3 && 
+                <>
+                    <button className="mx-2 h-9 max-h-[40px] w-9 max-w-[40px] select-none rounded-lg  text-center align-middle font-sans text-xs font-medium uppercase text-black shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-orange focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" onClick={() => gotoPage(0)}>1</button> 
+                    <span className=" text-black w-[20px] h-[20px] text-center mx-5">...</span>
+                </>}
+
+                {Array.from({length: Math.min(7, pageCount)}, (_, i) => Math.max(0, Math.min(pageCount - 7, pageIndex - 3)) + i).map(page => 
+                (page >= 0 && page < pageCount) ? (
+                    <button 
+                    key={page} 
+                    onClick={() => gotoPage(page)} 
+                    className={` mx-2 h-9 max-h-[40px] w-9 max-w-[40px] select-none rounded-lg  text-center align-middle font-sans text-xs font-medium uppercase text-black shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-orange focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ${page === pageIndex ? 'active-page-class bg-orange text-white' : ''}`}
+                    >
+                    {page + 1}
+                    </button>
+                ) : null
+                )}
+                {pageIndex < pageCount - 4 && 
+                <>
+                    <span className=" text-black w-[20px] h-[20px] text-center mx-5 ">...</span> 
+                    <button onClick={() => gotoPage(pageCount - 1)} className="mx-2 h-9 max-h-[40px] w-9 max-w-[40px] select-none rounded-lg  text-center align-middle font-sans text-xs font-medium uppercase text-black shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-orange focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">{pageCount}</button>
+                </>}
+
+
+                {/* Next button */}
+                <button     
+                    onClick={() => nextPage()} 
+                    disabled={!canNextPage}
+                    className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-orange
+                    disabled:bg-transparent disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none"
+                    type="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                    aria-hidden="true" className="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+                    </svg>
+                </button>{' '}
+
+
+                <span>
+                    Page{' '}
+                    <strong className=' text-black '>
+                    {pageIndex + 1} of {pageCount}
+                    </strong>{' '}
+                </span>
+
+           
+            </div>
+
+        </div>
+        
+    )
+}
+
+// function handleColumnClick(columnIndex:any) {
+//     switch(columnIndex) {
+//         case 0:
+//             console.log("0")
+//             break;
+//         case 1:
+//             console.log("1")
+//             break;
+//         case 2:
+//             console.log("2")
+//             break;
+//         case 3:
+//             console.log("3")
+//             break;
+//         case 4:
+//             console.log("4")
+//             break;
+// }
+//   }
+
